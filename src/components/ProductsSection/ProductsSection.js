9a86a38f2ca11ae717f3fa6d4732404c33a6d4f5
loren pabst <lorenpabst@gmail.com>
2017-07-10 14:13:24 -0600

    working on products section

--- src/components/ProductsSection/ProductsSection.js
+++ src/components/ProductsSection/ProductsSection.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './ProductsSection.css';
 
+import iphones from './../../img/phones.png';
+import watches from './../../img/watches.png';
+import imacs from './../../img/imacs.png';
+import accessories from './../../img/accessories.png';
+
 class ProductsSection extends Component {
   render() {
     return (
         <section className='products_section'>
 
           <div className='product_container iphones'>
+            <img src={ iphones } className='' />
           </div>
 
           <div className='product_container watches'>
+            <img src={ watches } className='watches' />
           </div>
 
           <div className='product_container imacs'>
+            <img src={ imacs } className='watches' />
           </div>
 
           <div className='product_container accessories'>
+            <img src={ accessories } className='watches' />
           </div>
 
         </section>
