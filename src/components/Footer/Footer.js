32e9b5f101b763f3966b6ba287423a6521faf160
loren pabst <lorenpabst@gmail.com>
2017-07-11 13:30:43 -0600

    moved top footer into its own component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
+import TopFooter from './TopFooter/TopFooter.js';
 import BottomFooter from './BottomFooter/BottomFooter.js';
 
 class Footer extends Component {
   render() {
     return (
+
         <section className='footer'>
-
-            <section className='top_footer'>
-
-            </section>
+            <TopFooter />
             <BottomFooter />
-            
         </section>
+        
     );
   }
 }
---
+++ src/components/Footer/TopFooter/TopFooter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './TopFooter.css';
+
+class TopFooter extends Component {
+  render() {
+    return (
+        <section className='top_footer'>
+          
+
+        </section>
+    );
+  }
+}
+
+export default TopFooter;
