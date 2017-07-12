360e6565029f449782224a46612796e2336840ca
loren pabst <lorenpabst@gmail.com>
2017-07-12 09:28:01 -0600

    mapping over the lists to create the footer topic elements

--- src/components/Footer/TopFooter/FooterTopic/FooterTopic.js
+++ src/components/Footer/TopFooter/FooterTopic/FooterTopic.js
@@ -6,30 +6,35 @@ class FooterTopic extends Component {
     super(props);
 
     this.state = {
-      shop: ['Mac', 'iPad', 'iPhone', 'Watch', 'TV', 'Music', 'iTunes', 'HomePod', 'iPod', 'Accessories', 'Gift Cards'],
-      store: ['Find a Store', 'Genius Bar', 'Today at Apple', 'Apple Camp', 'Field Trip', 'Apple Store App', 'Refurbished and Clearance', 'Financing', 'Reuse and Recycling', 'Order Status', 'Shopping Help'],
-      education: ['Apple and Education', 'Shop for College'],
-      business: ['Apple and Business', 'Shop for Business'],
-      account: [],
-      values: [],
-      about: [],
+      'shop': ['Shop and Learn', 'Mac', 'iPad', 'iPhone', 'Watch', 'TV', 'Music', 'iTunes', 'HomePod', 'iPod', 'Accessories', 'Gift Cards'],
+      'store': ['Find a Store', 'Genius Bar', 'Today at Apple', 'Apple Camp', 'Field Trip', 'Apple Store App', 'Refurbished and Clearance', 'Financing', 'Reuse and Recycling', 'Order Status', 'Shopping Help'],
+      'education': ['Apple and Education', 'Shop for College'],
+      'business': ['Apple and Business', 'Shop for Business'],
+      'account': ['Manage Your Apple ID', 'Apple Store Account', 'iCloud.com'],
+      'values': ['Accessibility', 'Education', 'Environment', 'Inclusion and Diversity', 'Privacy', 'Supplier Responsibility'],
+      'about': ['Newsroom', 'Apple Leadership', 'Job Opportunities', 'Investors', 'Events', 'Contact Apple'],
     }
   }
   
   render() {
     let list = null;
-    if (this.props.showList === 'shop'){
-      list = 
-        <section className='footer_topic'>
-          <h1>Shop and Learn</h1>
-          <p>
-            
-        </section>
-    }
+    let showList = this.props.showList;
+    
+    list = 
+      <div>
+        <h1>{ this.state[showList][0] }</h1>
+        {
+          this.state[showList].map( (item, i) => {
+            return <p key={i} >{item}</p>
+          })
+        }
+          
+      </div>
+    
     return (
         <section className='footer_topic'>
 
-            
+          { list }
             
         </section>
     );
